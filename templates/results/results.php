c73f018f0788f06ef1e55c9142359da96996efa0
João Andrade <jm.andrade04@gmail.com>
2019-03-16 20:08:01 +0000

    moved dbpedia results data parsing to another class

--- templates/results/results.php
+++ templates/results/results.php
@@ -1,11 +1,12 @@
 <div class="card padding-2">
     <div class="row">
         <?php
-        $term = $_GET['medical-specialty'];
-        $term = ucfirst($term);
-        $command = "curl \"http://dbpedia.org/sparql/?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=SELECT+%3Furi+%3Fname+where+%7B%0D%0A+%3Furi+a+dbo%3ADisease+.%0D%0A+%3Furi+dbp%3Afield+dbr%3A" . $term . "+.%0D%0A+%3Furi+foaf%3Aname+%3Fname%0D%0A%7D+LIMIT+10&format=text%2Fhtml&CXML_redir_for_subjs=121&CXML_redir_for_hrefs=&timeout=30000&debug=on&run=+Run+Query+\" > resources/dbpedia/" . $term . ".txt";
-        passthru($command);
-        echo $command;
+        require "/home/aw002/public_html/search-engine-diseases/php/dbpedia_data_retrieval.php";
+
+        $dbPediaData = new DbPediaData($_GET['medical-specialty']);
+        $dbPediaData->saveParsedContentToFile();
+        echo $dbPediaData->getFileContents();
+        echo '<pre>' . $dbPediaData->didContentChange . '</pre>';
         ?>
     </div>
 </div>
