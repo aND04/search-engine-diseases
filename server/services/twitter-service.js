b2c4431bc5453d6c4a6f37ba6be9475774999b0d
João Andrade <jm.andrade04@gmail.com>
2019-04-16 10:48:06 +0100

    improved twitter update

--- server/services/twitter-service.js
+++ server/services/twitter-service.js
@@ -2,29 +2,36 @@ const dbConnector = require('../db/db-connector');
 const stringUtils = require('../utils/string-utils');
 
 const getTweetId = async function (tweetId) {
-    return await dbConnector.query(`SELECT * FROM twitter_tweet where tweet_id = '${tweetId}'`);
+    const queryResult = await dbConnector.query(`SELECT * FROM twitter_tweet where tweet_id = '${tweetId}'`);
+    return queryResult[0] ? queryResult[0].id : -1;
 };
 
-const updateTweetDisease = async function (tweetId, diseaseId) {
-    const disease_tweet = await dbConnector.query(`SELECT * from twitter_tweet_dbpedia_disease where twitter_tweet_id = ${tweetId} and dbpedia_disease_id = ${diseaseId}`);
-    return !disease_tweet[0];
+const tweetExistsForCurrentDisease = async function (tweetId, diseaseId) {
+    const queryResult = await dbConnector.query(`SELECT * FROM twitter_tweet_dbpedia_disease WHERE twitter_tweet_id = ${tweetId} AND dbpedia_disease_id = ${diseaseId}`);
+    return !!queryResult[0];
+};
+
+const createRelationshipToDisease = async function (tweetId, diseaseId) {
+    await dbConnector.query(`INSERT INTO twitter_tweet_dbpedia_disease(twitter_tweet_id, dbpedia_disease_id) VALUES (${tweetId}, ${diseaseId})`);
+};
+
+const setUpdatedAtTweetValue = async function (tweetId) {
+    await dbConnector.query(`UPDATE twitter_tweet SET updated_at = NOW() WHERE id = ${tweetId}`);
 };
 
 const saveTweetToDb = async function (tweetId, description, tweetDate, url, diseaseId) {
-    const tweet = await getTweetId(tweetId);
-    const exists = await !!tweet[0];
-    if (!exists) {
-        const sanitizedDescription = await stringUtils.sanitize(description);
+    const id = await getTweetId(tweetId);
+    if (id === -1) {
+        const sanitizedDescription = stringUtils.sanitize(description);
         const encodedDescription = stringUtils.encodeBase64(sanitizedDescription);
         const queryResult = await dbConnector.query(`INSERT INTO twitter_tweet(tweet_id, url, description, tweet_date) VALUES ('${tweetId}', '${url}', '${encodedDescription}', '${tweetDate}')`);
-        const insertedId = queryResult.insertId;
-        await dbConnector.query(`INSERT INTO twitter_tweet_dbpedia_disease(twitter_tweet_id, dbpedia_disease_id) VALUES (${insertedId}, ${diseaseId})`);
+        const tweetInsertedId = queryResult.insertId;
+        await createRelationshipToDisease(tweetInsertedId, diseaseId);
+    } else if (tweetExistsForCurrentDisease(id, diseaseId)) {
+        await setUpdatedAtTweetValue(id);
     } else {
-        const id = tweet[0].id;
-        const update = await updateTweetDisease(id, diseaseId);
-        if (update) {
-            await dbConnector.query(`INSERT INTO twitter_tweet_dbpedia_disease(twitter_tweet_id, dbpedia_disease_id) VALUES (${id}, ${diseaseId})`);
-        }
+        await setUpdatedAtTweetValue(id);
+        await createRelationshipToDisease(id, diseaseId);
     }
 
 };
