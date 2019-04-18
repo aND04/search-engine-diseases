87620d34aea6e51047f75bb0394589f6670b9fdc
V1ctor1996 <victor_061996@hotmail.com>
2019-04-18 17:21:14 +0100

    Added the tables to store Dbpedia metadata.

--- server/app/db/sql/dbpedia.sql
+++ server/app/db/sql/dbpedia.sql
@@ -19,3 +19,27 @@ create table dbpedia_disease
   unique key UK_description (description),
   constraint fk_specialty_disease foreign key (medical_specialty_id) references dbpedia_medical_specialty(id)
 );
+
+create table dbpedia_metadata_disease
+(
+    id           bigint(20) not null auto_increment,
+    uri          varchar(255) not null,
+    diseaseName  varchar(255) not null,
+    image        varchar(255) not null,
+    diseaseField varchar(255) not null,
+    wikipageId   bigint(20)       not null,
+    deathNames   varchar(255) not null,
+    created_at  timestamp     not null default current_timestamp,
+    updated_at  timestamp,
+    primary key (id),
+    unique key uk_wikipageId (wikipageId)
+);
+
+create table dbpedia_metadata_dbpedia_disease
+(
+    dbpedia_metadata_id bigint(20) not null,
+    dbpedia_disease_id  bigint(20) not null,
+    primary key (dbpedia_metadata_id, dbpedia_disease_id),
+    constraint fk_dbpedia_metadata_dbpedia_disease_dbpedia_disease_id   foreign key (dbpedia_disease_id) references dbpedia_disease (id),
+    constraint fk_dbpedia_metadata_dbpedia_disease_dpedia_metadata_id   foreign key (dbpedia_metadata_id) references dbpedia_metadata_disease (id)
+);
