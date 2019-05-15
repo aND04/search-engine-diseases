d2ca6a9e77b2082a99b69d30e8fa5f95047018d9
linofnunes <linofnunes@gmail.com>
2019-05-15 22:44:32 +0100

    comentários no ajax

--- webapp/public/javaScript/ajax.js
+++ webapp/public/javaScript/ajax.js
@@ -19,9 +19,7 @@ var diseaseResource = "disease";
 function post() {
     // vai buscar o id registo e vai enviar para dentro do post
     var data = $("#disease").serialize();
-    let diseaseName = $("#myInput").val();
-
-    console.log(data);
+    var diseaseName = $("#myInput").val();
 
     //pedido AJAX artigo
     $.ajax({
@@ -29,8 +27,11 @@ function post() {
         data: data,
         method: "POST",
         success: function (result_article) {
-            console.log(result_article);
-            let titles = '<th>Title</th><th>Abstract</th><th>Pubmed</th><th>Date</th>';
+
+            //criar o titulo das colunas
+            var titles = '<th>Title</th><th>Abstract</th><th>Pubmed</th><th>Date</th>';
+
+            //escreve as linhas na tabela
             var trHTML = ''
             $.each(result_article, function (i, item) {
                 trHTML += '<tr><td>' + decodeURIComponent(escape(window.atob(item.title))) +
@@ -48,20 +49,24 @@ function post() {
                     '</td><td> <a target="_blank" href= https://www.ncbi.nlm.nih.gov/pubmed/' + item.pubmed_id + '> https://www.ncbi.nlm.nih.gov/pubmed/ ' + item.pubmed_id + '</a></td>' +
                     '<td>' + item.pub_Date + '</td></tr>';
             });
-            //$('#article').update(trHTML);
+
+            //apaga as linhas na tabela
             $('#article').empty().append(titles).append(trHTML);
 
+            // limpar as linhas
+            $('#article').empty().append(titles).append(trHTML);
+
+            //função para o hifhlight
             function getWordsNumber(){
+
                 let inputText = document.getElementById("article");
                 let innerHTML = inputText.innerHTML;
-                //let inputText = "asthma is bad asthma Asthma"
+                let word = innerHTML.match(new RegExp(diseaseName, "igm"));
+                let numberOfOccurrences = innerHTML.match(new RegExp(diseaseName, "igm")).length;
 
-                let palavra = innerHTML.match(new RegExp(diseaseName, "igm"));
-                //console.log("palavra: " + palavra);
-                const numberOfOccurrences = innerHTML.match(new RegExp(diseaseName, "igm")).length;
-                console.log("occorences: " + numberOfOccurrences);
+                //ciclo for para sublinhar o nome da doença no artigo
                 for(let i = 0; i<numberOfOccurrences; i++){
-                    let res = innerHTML.replace(new RegExp(diseaseName, "igm"), "<span class='highlight' style='background-color:yellow;'>" + palavra[i]+"</span>");
+                    let res = innerHTML.replace(new RegExp(diseaseName, "igm"), "<span class='highlight' style='background-color:yellow;'>" + word[i]+"</span>");
                     innerHTML = res;
                 }
                 inputText.innerHTML = innerHTML;
@@ -72,19 +77,24 @@ function post() {
         }
     })
 
-    //pedido AJAX tweet
+    //pedido AJAX twitter
     $.ajax({
         url: domain + tweetResource,
         data: data,
         method: "POST",
 
         success: function (result_tweet) {
-            console.log(result_tweet);
-            let titles = '<tr> <em>Twitter photos</em><th>Description</th> <th>URL</th> <th>Date</th> </tr>';
+
+            //criar o titulo das colunas
+            var titles = '<tr> <em>Twitter photos</em><th>Description</th> <th>URL</th> <th>Date</th> </tr>';
+
+            //escreve as linhas na tabela
             var trHTML = '';
             $.each(result_tweet, function (i, item) {
                 trHTML += '<tr><td>' + decodeURIComponent(escape(window.atob(item.description))) + '</td><td> <a target="_blank" href="' + item.url + '">  ' + item.url + '</a></td><td>' + item.tweet_date + '</td></tr>';
             });
+
+            // limpar as linhas
             $('#twitter').empty().append(titles).append(trHTML);
         }
 
@@ -98,13 +108,17 @@ function post() {
         method: "POST",
         success: function (result_flickr) {
 
-            console.log(result_flickr);
-            let titles = '<tr> <em>Flirck photos</em> <th>Title</th> <th>URL</th> <th>Date</th> </tr>';
+            //criar o titulo das colunas
+            var titles = '<tr> <em>Flirck photos</em> <th>Title</th> <th>URL</th> <th>Date</th> </tr>';
+
+            //escreve as linhas na tabela
             var trHTML = '';
             $.each(result_flickr, function (i, item) {
                 trHTML += '<tr><td>' + decodeURIComponent(escape(window.atob(item.title))) +
                     '</td><td> <a target="_blank" href="' + item.url + '">  ' + item.url + '</a></td><td>' + item.created_at + '</td></tr>';
             });
+
+            // limpar as linhas
             $('#flickr').empty().append(titles).append(trHTML);
         }
     })
@@ -116,14 +130,18 @@ function post() {
         method: "POST",
         success: function (result_metadata) {
 
-            console.log(result_metadata);
-            let titles = '<tr><em>More info</em><th>wikipageId</th><th>URI</th> <th>Image</th> <th>Comment</th></tr>';
+            //criar o titulo das colunas
+            var titles = '<tr><em>More info</em><th>wikipageId</th><th>URI</th> <th>Image</th> <th>Comment</th></tr>';
+
+            //escreve as linhas na tabela
             var trHTML = '';
             $.each(result_metadata, function (i, item) {
                 trHTML += '<tr><td>' + item.wikipageId +
                     '</td><td> <a target="_blank" href="' + item.uri + '">  ' + item.uri + '</a></td><td>  ' +
                     '<img src="' + item.image + '" width="420" height="420"> </td><td>' + decodeURIComponent(escape(window.atob(item.comment))) + '</td></tr>';
             });
+
+            // limpar as linhas
             $('#metadata').empty().append(titles).append(trHTML);
 
         }
@@ -135,14 +153,18 @@ function post() {
         data: data,
         method: "POST",
         success: function (result_disease) {
-            console.log(result_disease);
 
-            let titles = '<tr> <em>Related Diseases</em> <th>Id</th> <th>Disease Name</th></tr>';
+            //criar o titulo das colunas
+            var titles = '<tr> <em>Related Diseases</em> <th>Id</th> <th>Disease Name</th></tr>';
+
+            //escreve as linhas na tabela
             var trHTML = '';
             $.each(result_disease, function (i, item) {
                 trHTML += '<tr><td>' + item.dbpedia_disease_id +
                     '</td><td>'+ item.description + '</td></tr>';
             });
+
+            // limpar as linhas
             $('#related_diseases').empty().append(titles).append(trHTML);
         }
 
