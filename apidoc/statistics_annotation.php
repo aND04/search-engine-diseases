3dcda664afb844bd74c5354c6db24aa50d9837c9
MissGorgeousTech <46816885+MissGorgeousTech@users.noreply.github.com>
2019-05-19 19:39:14 +0100

    Added statistics pages to apidocs

---
+++ apidoc/statistics_annotation.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <script src="js/materialize.js"></script>
+        <style>
+    		table, th, td {
+    		    border-collapse: collapse;
+                border-spacing: 0;
+                width: 100%;
+    		    border: 1px solid black;
+    		}
+
+    		th, td {
+    		    text-align: center;
+    			border: 1px solid black;
+    			padding: 8px;
+    		}
+        </style>
+    </head>
+    <body class="grey lighten-5">
+        <header>
+            <div class="row">
+                <div class="col s12">
+                  <ul class="tabs">
+                    <li class="tab col s3"><a class="active" href="/index.html">API</a></li>
+                    <li class="tab col s3"><a href="/statistics_retrieval.php">Statistics Demo 1</a></li>
+                    <li class="tab col s3"><a href="/statistics_annotation.php">Statistics Demo 2</a></li>
+                  </ul>
+                </div>
+            </div>
+        </header>
+        <main>
+        <h1>Statistics - Demo II</h1>
+        <form action='statistics_annotation.php' method='get'>
+            <p> Disease: <input type='text' name='disease' /> </p>
+            <p><input type='submit' value='Submit'/> </p>
+        </form>
+        <hr>
+
+<?php
+	include "config.php";
+
+    if (!empty ($_GET)) {
+         //Get disease id
+         $disease = ucfirst($_GET['disease']);
+         $sql = "SELECT id FROM dbpedia_disease WHERE description = '$disease'";
+         $result = $conn->query($sql);
+         $row = $result -> fetch_assoc();
+         $diseaseId = $row["id"];
+        if (mysqli_num_rows($result) == 0) {
+            echo "Disease not found!";
+        } else {
+            //Articles and statistics
+            $sql = "SELECT title, doidDisease,number_of_mentions, tfidf, similarity, explicitFeedbackValue, implicitFeedbackValue, date_Relevance, relevance_Avg
+                    FROM pubmed_article_dbpedia_disease
+            	    INNER JOIN pubmed_article on pubmed_article.id = pubmed_article_dbpedia_disease.pubmed_article_id
+            	    INNER JOIN relevance on relevance.pubmed_article_id = pubmed_article.id and relevance.dbpedia_disease_id = pubmed_article_dbpedia_disease.dbpedia_disease_id
+            	    INNER JOIN pubmed_article_dbpedia_disease_mentions on pubmed_article_dbpedia_disease_mentions.pubmed_article_id = pubmed_article.id and pubmed_article_dbpedia_disease_mentions.dbpedia_disease_id = pubmed_article_dbpedia_disease.dbpedia_disease_id
+            	    WHERE pubmed_article_dbpedia_disease.dbpedia_disease_id = '$diseaseId'
+            	    ORDER BY relevance_Avg desc";
+            $result = $conn->query($sql);
+            if (mysqli_num_rows($result) == 0) {
+                echo "Articles not found!";
+            } else {
+                echo "<div style='overflow-x:auto;'>";
+                echo "<table>";
+                echo "<tr>";
+                echo "<th>Article title</th>";
+                echo "<th>DOID</th>";
+                echo "<th>Number of mentions</th>";
+                echo "<th>TFIDF</th>";
+                echo "<th>Similarity</th>";
+                echo "<th>Explicit feedback</th>";
+                echo "<th>Implicit feedback</th>";
+                echo "<th>Date relevance";
+                echo "<th>Weighted average</th>";
+
+                $rowArticles = array();
+                while($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+                	$rowArticles[] = $row;
+                }
+                foreach ($rowArticles as $article) {
+                	echo "<tr>";
+                	echo "<td>". base64_decode($article['title']) . "</td>";
+                	echo "<td>". $article['doidDisease'] . "</td>";
+                	echo "<td>". $article['number_of_mentions'] . "</td>";
+                	echo "<td>". $article['tfidf'] . "</td>";
+                	echo "<td>". $article['similarity'] . "</td>";
+                	echo "<td>". $article['explicitFeedbackValue'] . "</td>";
+                	echo "<td>". $article['implicitFeedbackValue'] . "</td>";
+                	echo "<td>". $article['date_Relevance'] . "</td>";
+                	echo "<td>". $article['relevance_Avg'] . "</td>";
+                	echo "</tr>";
+                }
+                echo "</table>";
+                echo "</div>";
+            }
+        }
+    }
+
+    $conn->close();
+?>
+      </main>
+    </body>
+</html>
---
+++ apidoc/statistics_retrieval.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <script src="js/materialize.js"></script>
+		</head>
+		<body class="grey lighten-5">
+				<header>
+						<div class="row">
+								<div class="col s12">
+									<ul class="tabs">
+										<li class="tab col s3"><a class="active" href="/index.html">API</a></li>
+										<li class="tab col s3"><a href="/statistics_retrieval.php">Statistics Demo 1</a></li>
+										<li class="tab col s3"><a href="/statistics_annotation.php">Statistics Demo 2</a></li>
+									</ul>
+								</div>
+						</div>
+				</header>
+				<main>
+					<h1>Statistics - Demo I</h1>
+				    <form action='statistics_retrieval.php' method='get'>
+		            <p> Specialty: <input type='text' name='specialty' /> </p>
+		            <p><input type='submit' value='Submit'/> </p>
+		        </form>
+				    <hr>
+<?php
+include "config.php";
+
+//Get specialty ID
+$specialty = isset($_GET['specialty']) ? $_GET['specialty'] : '';
+$sql = "SELECT id FROM dbpedia_medical_specialty WHERE description = '$specialty'";
+$result = $conn->query($sql);
+$row = $result -> fetch_assoc();
+$idSpecialty = $row["id"];
+
+//Number of diseases
+$sql = "SELECT COUNT(*) AS total FROM dbpedia_disease WHERE medical_specialty_id = '$idSpecialty'";
+$result = $conn->query($sql);
+$row = $result -> fetch_assoc();
+echo "Number of diseases: ". $row['total']. "<br>";
+
+//Number of articles
+$sql = "SELECT COUNT(*) AS total FROM pubmed_article INNER JOIN pubmed_article_dbpedia_disease padd ON pubmed_article.id = padd.pubmed_article_id, dbpedia_disease disease WHERE disease.medical_specialty_id = '$idSpecialty' AND disease.id = padd.dbpedia_disease_id";
+$result = $conn->query($sql);
+$row = $result -> fetch_assoc();
+echo "Number of articles: ". $row['total']. "<br>";
+
+//Number of flickr photos
+$sql = "SELECT COUNT(fp.flickr_id) AS total FROM flickr_photo fp INNER JOIN flickr_photo_dbpedia_disease fpdd ON fp.id = fpdd.flickr_photo_id INNER JOIN dbpedia_disease dd ON fpdd.dbpedia_disease_id = dd.id INNER JOIN dbpedia_medical_specialty dms ON dd.medical_specialty_id = dms.id WHERE dms.description = '$specialty'";
+$result = $conn->query($sql);
+$row = $result -> fetch_assoc();
+echo "Number of flickr photos: ". $row['total']. "<br>";
+
+//Number of tweets
+$sql = "SELECT COUNT(DISTINCT t.id) AS total FROM twitter_tweet t INNER JOIN twitter_tweet_dbpedia_disease ttdd ON t.id = ttdd.twitter_tweet_id INNER JOIN dbpedia_disease dd ON ttdd.dbpedia_disease_id = dd.id INNER JOIN dbpedia_medical_specialty dms ON dd.medical_specialty_id = dms.id WHERE dms.description = '$specialty'";
+$result = $conn->query($sql);
+$row = $result -> fetch_assoc();
+echo "Number of tweets: ". $row['total']. "<br>";
+
+$conn->close();
+?>
+</main>
+</body>
+</html>
